33b84b7bd9e34b80632cd6ebb6f6268480cdea72
Manuel <nery23293@gmail.com>
2017-04-19 14:20:29 -0500

    lokote

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -6,14 +6,14 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-dashboard',
-  template: '<h3>My dashboard</h3>'
+  templateUrl: './dashboard.component.html',
+  providers: [HeroService]
 })
 
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
   constructor(private heroService: HeroService) { }
   ngOnInit(): void {
-    this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1, 5));
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 }
